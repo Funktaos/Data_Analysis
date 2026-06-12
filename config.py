bb304511ad1905cb206737515284c7093aea2025
Funktaos <levin.rfn.ru@gmail.com>
2026-06-12 10:57:32 +0200

    Mask sensitive information in config.py
    Updated database and MongoDB configuration with masked passwords.

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 dbconfig = {
     "host": "ich-db.edu.itcareerhub.de",
     "user": "ich1",
-    "password": "password_ich1",
+    "password": "***************",
     "database": "sakila",
 }
 
 mongo_config = (
-    "mongodb://ich_editor:verystrongpassword"
+    "mongodb://ich_editor:v***********password"
     "@mongo.itcareerhub.de/?readPreference=primary"
     "&ssl=false&authMechanism=DEFAULT&authSource=ich_edit"
 )
